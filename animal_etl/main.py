51f9c236e8e207716d6bea9e485044130bfcc9c0
asif-usman <usmanqureshi2201@gmail.com>
2025-07-29 07:55:39 +0500

    Add main ETL entrypoint also changes the settings of project

---
+++ animal_etl/main.py
@@ -0,0 +1,20 @@
+from etl.client import AnimalAPIClient
+from etl.transform import transform_animal
+from etl.loader import load_animals_in_batches
+
+def main():
+    client = AnimalAPIClient()
+    all_animals = []
+
+    print("Fetching animal listings...")
+    for animal_id in client.get_all_animal_ids():
+        details = client.get_animal_details(animal_id)
+        if details:
+            transformed = transform_animal(details)
+            all_animals.append(transformed)
+
+    print(f"Fetched and transformed {len(all_animals)} animals.")
+    load_animals_in_batches(client, all_animals)
+
+if __name__ == "__main__":
+    main()
